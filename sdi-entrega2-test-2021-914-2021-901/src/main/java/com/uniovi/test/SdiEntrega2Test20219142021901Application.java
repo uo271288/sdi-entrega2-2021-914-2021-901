ba100f72761b1f825e3963249656e43886857f8c
Alejandro <uo271288@uniovi.es>
2021-05-02 16:18:44 +0200

    Primer commit pruebas

---
+++ sdi-entrega2-test-2021-914-2021-901/src/main/java/com/uniovi/test/SdiEntrega2Test20219142021901Application.java
@@ -0,0 +1,13 @@
+package com.uniovi.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega2Test20219142021901Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiEntrega2Test20219142021901Application.class, args);
+	}
+
+}
---
+++ sdi-entrega2-test-2021-914-2021-901/src/test/java/com/uniovi/test/SdiEntrega2Test20219142021901ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiEntrega2Test20219142021901ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
