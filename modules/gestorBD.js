77fbb6f0bc2db92239e5dac823fa53830be3d3eb
UO271588 <uo271588@uniovi.es>
2021-05-06 22:53:42 +0200

    sdi-2021-914-2021-901-1.4-Añadida ventana mis ofertas eliminar ofertas comprar ofertas

--- modules/gestorBD.js
+++ modules/gestorBD.js
@@ -104,6 +104,23 @@ module.exports = {
             }
         });
     },
+    eliminarOferta : function(criterio, funcionCallback) {
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('ofertas');
+                collection.remove(criterio, function(err, result) {
+                    if (err) {
+                        funcionCallback(null);
+                    } else {
+                        funcionCallback(result);
+                    }
+                    db.close();
+                });
+            }
+        });
+    },
     obtenerOfertas: function (criterio, funcionCallback) {
         this.mongo.MongoClient.connect(this.app.get('db'), function (err, db) {
             if (err) {
@@ -134,6 +151,26 @@ module.exports = {
                         funcionCallback(result.ops[0]._id);
                     }
                     db.close();
+                });
+            }
+        });
+    },
+    obtenerOfertasPg : function(criterio,pg,funcionCallback){
+        this.mongo.MongoClient.connect(this.app.get('db'), function(err, db) {
+            if (err) {
+                funcionCallback(null);
+            } else {
+                let collection = db.collection('ofertas');
+                collection.count(function(err, count){
+                    collection.find(criterio).skip( (pg-1)*4 ).limit( 5 )
+                        .toArray(function(err, ofertas) {
+                            if (err) {
+                                funcionCallback(null);
+                            } else {
+                                funcionCallback(ofertas, count);
+                            }
+                            db.close();
+                        });
                 });
             }
         });
--- routes/rofertas.js
+++ routes/rofertas.js
@@ -4,22 +4,35 @@ module.exports = function(app, swig,gestorBD, logger) {
         if( req.query.busqueda != null ){
             criterio = { "titulo" : {$regex : ".*"+req.query.busqueda+".*"}  };
         }
-
-        gestorBD.obtenerOfertas( criterio,function(ofertas) {
+        let pg = parseInt(req.query.pg); // Es String !!!
+        if ( req.query.pg == null){ // Puede no venir el param
+            pg = 1;
+        }
+        gestorBD.obtenerOfertasPg(criterio, pg , function(ofertas, total ) {
             if (ofertas == null) {
-                res.send("Error al listar ");
+                res.send("Error al listar ofertas");
             } else {
+                let ultimaPg = total/5;
+                if (total % 5 > 0 ){ // Sobran decimales
+                    ultimaPg = ultimaPg+1;
+                }
+                let paginas = []; // paginas mostrar
+                for(let i = pg-2 ; i <= pg+2 ; i++){
+                    if ( i > 0 && i <= ultimaPg){
+                        paginas.push(i);
+                    }
+                }
                 let respuesta = swig.renderFile('views/ofertas.html',
                     {
                         ofertas : ofertas,
-                        usuario: req.session.usuario
+                        usuario: req.session.usuario,
+                        paginas : paginas,
+                        actual : pg
                     });
                 res.send(respuesta);
             }
         });
     });
-
-
 
     app.get('/ofertas/agregar', function (req, res) {
         let respuesta = swig.renderFile('views/agregar.html', {
@@ -29,19 +42,39 @@ module.exports = function(app, swig,gestorBD, logger) {
     });
 
     app.get("/ofertas/comprar/:id", function(req, res) {
-        //       let ofertaId = gestorBD.mongo.ObjectID(req.params.id);
-        //       let compra = {
-        //       ofertaId : ofertaId
-        //  }
-        // gestorBD.insertarCompra(compra ,function(idCompra){
-        //     if ( idCompra == null ){
-        //        res.send(respuesta);
-        //   } else {
-        //        res.redirect("/ofertas.html");
-        //   }
-        //});
-        res.redirect("/ofertas")
-
+        let ofertaId = gestorBD.mongo.ObjectID(req.params.id);
+               let compra = {
+                  usuario : req.session.usuario,
+                  ofertaId : ofertaId
+               }
+                gestorBD.insertarCompra(compra ,function(idCompra){
+                if ( idCompra == null ){
+                    res.send(respuesta);
+                } else {
+                    let criterio = { "_id" : gestorBD.mongo.ObjectID(ofertaId) };
+                    gestorBD.obtenerOfertas(criterio,function(ofertas) {
+                        if (ofertas == null) {
+                            res.send("Error al listar ofertas ");
+                        } else {
+                            let oferta = {
+                                titulo : ofertas[0].titulo,
+                                detalles : ofertas[0].detalles,
+                                precio : ofertas[0].precio,
+                                autor: ofertas[0].usuario,
+                                hora : ofertas[0].hora,
+                                comprador: req.session.usuario
+                            }
+                            gestorBD.modificarOferta(criterio, oferta, function(result) {
+                                if (result == null) {
+                                    res.send("Error al modificar ");
+                                } else {
+                                    res.redirect("/ofertas");
+                                }
+                            });
+                        }
+                    });
+                }
+        });
     });
 
 
@@ -50,6 +83,8 @@ module.exports = function(app, swig,gestorBD, logger) {
             titulo : req.body.titulo,
             detalles : req.body.detalles,
             precio : req.body.precio,
+            autor: req.session.usuario,
+            hora : new Date(Date.now()),
             comprador: null
         }
         // Conectarse
@@ -64,4 +99,55 @@ module.exports = function(app, swig,gestorBD, logger) {
         });
 
     });
+
+    app.get("/compras", function (req,res){
+       let criterio = {"usuario" : req.session.usuario};
+
+       gestorBD.obtenerCompras(criterio,function (compras) {
+           if(compras == null){
+               res.send("Error al listar");
+           }else{
+               let ofertasCompradasIds=[];
+               for(i=0; i<compras.length;i++){
+                   ofertasCompradasIds.push(compras[i].ofertaId);
+               }
+
+               let criterio = {"_id": {$in: ofertasCompradasIds}}
+               gestorBD.obtenerOfertas(criterio, function(ofertas){
+                   let respuesta = swig.renderFile('views/compras.html',{
+                     ofertas : ofertas
+                   });
+                   res.send(respuesta);
+               })
+           }
+       })
+    });
+
+    app.get("/ofertas/misofertas", function(req, res) {
+        let criterio = {"autor" : req.session.usuario};
+
+        gestorBD.obtenerOfertas( criterio,function(ofertas) {
+            if (ofertas == null) {
+                res.send("Error al listar ofertas ");
+            } else {
+                let respuesta = swig.renderFile('views/misofertas.html',
+                    {
+                        ofertas : ofertas,
+                        usuario: req.session.usuario
+                    });
+                res.send(respuesta);
+            }
+        });
+    });
+    app.get('/ofertas/eliminar/:id', function (req, res) {
+        let criterio = {"_id" : gestorBD.mongo.ObjectID(req.params.id) };
+        gestorBD.eliminarOferta(criterio,function(ofertas){
+            if ( ofertas == null ){
+                res.send(respuesta);
+            } else {
+                res.redirect("/ofertas/misofertas");
+            }
+        });
+    });
+
 };
